c4e3959220316e64eb253c7fa064aa79a67bd554
mdmhrz <razufreelance@gmail.com>
2025-11-08 02:28:44 +0600

    Stack created by javascript algorithm

---
+++ module-04-algorithm-in-action/stack.js
@@ -0,0 +1,56 @@
+export default class Stack {
+    constructor() {
+        this.items = []
+    }
+
+    // O(1)
+    push(value) {
+        this.items.push(value)
+    }
+
+    // O(1)
+    pop() {
+        if (this.isEmpty()) {
+            return undefined
+        }
+
+        return this.items.pop()
+    }
+
+    // O(1)
+    peak() {
+        if (this.isEmpty()) {
+            return undefined
+        }
+
+        return this.items[this.items.length - 1]
+    }
+
+    // O (1)
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    print() {
+        console.log(this.items.length > 0 ? this.items.slice().reverse().join("->") : this.items);
+    }
+}
+
+const stack = new Stack()
+
+// stack.print()
+
+// console.log(stack.isEmpty());
+
+// stack.push(10)
+// stack.push(20)
+// stack.push(30)
+// // stack.pop(4)
+
+// stack.print()
+
+// console.log(stack.peak())
+
+// stack.pop()
+
+// stack.print()
---
+++ module-04-algorithm-in-action/valid_parentheses.js
@@ -0,0 +1,11 @@
+import Stack from "./stack.js";
+
+// Problem Statement
+
+// Given a string containing just the characters,
+// Determine if the input string is valid
+
+const stack = new Stack()
+
+stack.push(10)
+stack.print()
