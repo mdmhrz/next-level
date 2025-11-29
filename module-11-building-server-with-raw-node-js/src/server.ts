55e45f2b272bdd3685f5fc4035b2838f80ae8fcb
mdmhrz <razufreelance@gmail.com>
2025-11-29 13:07:29 +0600

    Crud operations completed by raw node js

--- module-11-building-server-with-raw-node-js/src/server.ts
+++ module-11-building-server-with-raw-node-js/src/server.ts
@@ -1,21 +1,50 @@
 import http, { IncomingMessage, Server, ServerResponse } from 'http';
 import config from './config';
 
+
 const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
     console.log('Server is runnig');
 
+    //  root route get method
     if (req.url === '/' && req.method === 'GET') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({
             message: 'Hello from Node.js server!',
             path: req.url,
         }));
-    } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
+    }
+
+    //health route get method
+    if (req.url === '/api' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({
-            message: 'Route not found',
+            message: 'Health status ok',
+            path: req.url,
         }));
     }
+
+    if (req.url == '/api/users' && req.method == 'POST') {
+
+        let body = '';
+
+        //listen for data chunk
+        req.on('data', (chunk) => {
+            body += chunk.toString()
+        });
+
+
+        req.on('end', () => {
+            try {
+                const parseBody = JSON.parse(body);
+                console.log(parseBody);
+                console.log('catching current changes');
+                res.end(JSON.stringify(parseBody))
+            } catch (error: any) {
+                console.log(error?.message);
+            }
+        })
+    }
+
 })
 
 
