5301ae62b23f66c10c9f29da7e65d09208434b01
mdmhrz <razufreelance@gmail.com>
2025-12-05 12:05:54 +0600

    Server file added with new postgres table and dotenv professional configuration

--- module-12-crud-operation-with-postgreSQL/src/server.ts
+++ module-12-crud-operation-with-postgreSQL/src/server.ts
@@ -1,15 +1,20 @@
-import express, { Request, Response } from "express"
-import { Pool } from "pg"
-import 'dotenv/config';
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { Pool } from "pg";
+import path from "path";
 
 
-const app = express()
-const port = 5000
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+
+const app = express();
+const port = 5000;
 
 // postgreSQL DB connection pool
 const pool = new Pool({
-  connectionString: process.env.PG_CONNECTION_STRING
-})
+  connectionString: `${process.env.PG_CONNECTION_STRING}`
+});
+
 
 const initDB = async () => {
   await pool.query(`
@@ -23,31 +28,43 @@ const initDB = async () => {
       created_at TIMESTAMP DEFAULT NOW(),
       updated_at TIMESTAMP DEFAULT NOW()
     )
-    `)
-}
+    `);
 
-initDB()
+  await pool.query(`
+      CREATE TABLE IF NOT EXISTS todos(
+      id SERIAL PRIMARY KEY,
+      user_id INT REFERENCES users(id) ON DELETE CASCADE,
+      title VARCHAR(200) NOT NULL,
+      description TEXT,
+      completed BOOLEAN DEFAULT false,
+      due_date DATE,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+      `)
+};
+
+initDB();
 
 
 
 // Parser
-app.use(express.json())
-// app.use(express.urlencoded())
+app.use(express.json());
 
 
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello Next Level World!')
-})
+  res.send('<h1>Hello</h1>')
+});
 
 app.post("/", (req: Request, res: Response) => {
-  // console.log(req.body);
+
   res.status(500).json({
     success: true,
     message: "API is working"
   })
-})
+});
 
 app.listen(port, () => {
   console.log(`server is running on port: ${port}`)
-})
+});
